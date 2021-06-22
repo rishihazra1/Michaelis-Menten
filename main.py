2e78be92a87869be3bb41d86d0149bc0d6bd7f9e
Rishi Hazra <hazrarish@gmailcom>
2021-06-22 00:14:58 -0700

    complete: 		a. User inputs number of trials conducted 		b. User inputs desired truncation (for linearity) for each file

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import os
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 
-print("Enter molar extinction coefficient.")
+print("Enter molar extinction coefficient of your substrate.")
 molarExtinction = float(input())
 
 print("How many concentrations did you run trials for?")
-trialsRun = int(input())
+concentrationsRun = int(input())
 print("Enter the concentrations (in µM) at which you ran trials.")
 trialConcentrations = []
 t = 0
-while t < trialsRun:
+while t < concentrationsRun:
     trialConcentrations.append(input())
     t += 1
 
 print(trialConcentrations)
-
-print("Please select the folder containing your UV-Visible Light Spectroscopy Data")
-path = askdirectory(title='Select Folder')  # shows dialog box and return the path
-print(path)
+n = 0
+while n < concentrationsRun:
+    truncationTimes = []
+    print("Enter your desired start time and end time in seconds (pressing enter between each time) for your " + trialConcentrations[n] + " µM trials.")
+    while len(truncationTimes) < 2:
+        truncationTimes.append(input())
+    print("Select the folder containing your data at " + trialConcentrations[n] + " µM")
+    path = askdirectory(title='Select Folder')  # shows dialog box and return the path
+    print(path)
+    n += 1
 
