089dc12ee47ea689f1ad58d2fe5cdf4e7dff2985
Rishi Hazra <hazrarish@gmailcom>
2021-06-21 23:40:11 -0700

    complete: User inputs molar extinction coefficient of substrate User inputs concentrations UV spectrophotometer was run at

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from tkinter.filedialog import askdirectory
 print("Enter molar extinction coefficient.")
 molarExtinction = float(input())
 
+print("How many concentrations did you run trials for?")
+trialsRun = int(input())
+print("Enter the concentrations (in µM) at which you ran trials.")
+trialConcentrations = []
+t = 0
+while t < trialsRun:
+    trialConcentrations.append(input())
+    t += 1
+
+print(trialConcentrations)
+
 print("Please select the folder containing your UV-Visible Light Spectroscopy Data")
 path = askdirectory(title='Select Folder')  # shows dialog box and return the path
 print(path)
-
-os.chdir(path)
-
-def read_text_file(file_path):
-    with open(file_path, 'r') as f:
-        print(f.read())
-
-
-# iterate through all file
-for file in os.listdir():
 
