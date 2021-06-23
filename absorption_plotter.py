8940f896061425a2a0135a6cb82967c6c60ab3de
rishihazra1 <54484447+rishihazra1@users.noreply.github.com>
2021-06-22 21:27:13 -0700

    updated: plot absorption function created

--- absorption_plotter.py
+++ absorption_plotter.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt
+import file_interpretation
 
-def plot_
+start_time = int()
+end_time = int()
+user_input_concentrations = []
+
+
+def plot_absorption(file, start, end, concentrations):
+    x_values = concentrations
+    y_values = []
+    for temp_time in range(start, end + 1):
+        y_values.append(file_interpretation.get_absorption(file, temp_time))
+    plt.scatter(x_values, y_values)
+    plt.show()
