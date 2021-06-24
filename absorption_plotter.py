8f9e8f16d03f7115fd45f055d9a8faec2375e100
rishihazra1 <54484447+rishihazra1@users.noreply.github.com>
2021-06-23 23:32:25 -0700

    updated: option enabled not to select range when plotting file

--- absorption_plotter.py
+++ absorption_plotter.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 import file_interpretation
 
-start_time = int()
-end_time = int()
 
-
-def plot_single_file(file, start, end):
+def plot_single_file(file):
     first_column, second_column = file_interpretation.read_file(file)
     all_y_values = file_interpretation.initialize_array(first_column, second_column)
     x_values = []
     y_values = []
+    yes_or_no = input("Would you like to select a specific range to plot?\n")
+    if str.lower(yes_or_no) == "no":
+        start = 0
+        end = len(all_y_values) - 1
+    else:
+        start = int(input("Enter your desired start time for the plot.\n"))
+        end = int(input("Enter your desired end time for the plot.\n"))
     index = start
     while index <= end:
         try:
@@ -17,7 +21,8 @@ def plot_single_file(file, start, end):
             index += 1
         except IndexError:
             print(
-                "Index Error. Data point(s) are missing in given file. Plot will be truncated to the length of your data set. ")
+                "Index Error. Data point(s) are missing in given file. Plot will be truncated to the length of your "
+                "data set. ")
             break
     for time in range(start, index):
         x_values.append(time)
