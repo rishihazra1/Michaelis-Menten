3f19cf9c1afc32d3ecdd5ef1441ccae59e612406
rishihazra1 <54484447+rishihazra1@users.noreply.github.com>
2021-07-07 11:03:41 -0700

    alternate data_tracker code

--- data_tracker.py
+++ data_tracker.py
@@ -1,50 +1,67 @@
+in_data = input("Enter the current substrate concentration. Enter 'no' to exit "
+                "loop.\n")
+concentrations = {}
+
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+
+while not in_data == "no":
+    concentration = float(in_data)
+    concentrations[concentration] = []
+    next_occurrence = input("Enter trial\n")
+    while not next_occurrence == "no":
+        keep_value = str2bool(input("keep?"))
+        concentrations[concentration].append((float(next_occurrence), keep_value))
+        next_occurrence = input("Enter trial\n")
+    in_data = input("Enter the current substrate concentration. Enter 'no' to exit "
+                    "loop.\n")
+print(concentrations)
+
+for i, concentration in enumerate(concentrations.keys()):
+    print("data for trial: ", str(i))
+    for j, trial in enumerate(concentrations[concentration]):
+        trial_value, trial_validity = trial
+        print("trial" + str(j) + "value: ", trial_value)
+        print("trial" + str(j) + "validity: ", trial_validity)
+
+
 def track_data():
+    continue_running = True
     concentrations = []
     trials_per_concentration = []
     data = []
-    trial_validity = []
-    while True:  # terminates when there are no more concentrations
-        while True:  # terminates with valid user input
-            next_occurrence = input("Enter the current substrate concentration (in µM). Enter 'no' to exit "
+    while continue_running is True:
+        concentration_valid = False
+        while concentration_valid is False:
+            next_occurrence = input("Enter the current substrate concentration. Enter 'no' to exit "
                                     "loop.\n")
             if next_occurrence == "no":
-                break
-            try:
-                concentrations.append(float(next_occurrence))
-                break
-            except ValueError:
-                print("Input not recognized. Enter the concentration or enter 'no' to exit loop.")
-        if next_occurrence == "no":
-            break
-        current_trial = 0
-        while True:  # terminates when there are no more trials for current concentration
-            current_trial += 1
-            while True:
-                next_trial_input = input("Enter data for Trial " + str(current_trial) + " at " + str(
-                    next_occurrence) + " µM. Enter 'no' to exit loop.\n")
-                if next_trial_input == "no":
-                    current_trial -= 1
-                    break
+                concentration_valid = True
+            else:
                 try:
-                    data.append(float(next_trial_input))
-                    break
-                except ValueError:
-                    print("Input not recognized. Enter the data value or enter 'no' to exit loop.")
-            if next_trial_input == "no":
-                break
-            while True:  # terminates when user input for trial_validity is valid
-                keep_trial = input("Would you like to keep the data for " + str(current_trial) + " ? (yes/no)\n")
-                if keep_trial == "yes":
-                    trial_validity.append(True)
-                    break
-                elif keep_trial == "no":
-                    trial_validity.append(False)
-                    break
-                else:
+                    concentrations.append(next_occurrence)
+                    concentration_valid = True
+                except TypeError:
                     print("Input not recognized. Enter the concentration or enter 'no' to exit loop.")
-        trials_per_concentration.append(int(current_trial))
+                    concentration_valid = False
+                    break
+        current_trial = 0
+        are_more_trials = True
+        while are_more_trials is True:
+            current_trial += 1
+            next_trial_input = input("Enter data for Trial " + str(current_trial) + " at " + str(
+                next_occurrence) + ". Enter 'no' to exit loop.\n")
+            if next_trial_input == "no":
+                current_trial -= 1
+                are_more_trials = False
+            else:
+                data.append(next_trial_input)
+        trials_per_concentration.append(current_trial)
     print("concentrations: " + str(concentrations))
     print("trials_per_concentration: " + str(trials_per_concentration))
-    print("trial_validity statuses: " + str(trial_validity))
     print("Data stored in csv file *insert file path here*.")
-    return concentrations, trials_per_concentration, trial_validity  # eventually, return path of csv file instead
+    return concentrations, trials_per_concentration  # eventually, return path of csv file instead
+
+# track_data()
